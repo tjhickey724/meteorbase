a5d9567fad69dfa3f5c82ef08cd6a0f55534ffcb
Evan Druskin <evandruskin@Evans-MacBook-Pro.local>
2016-07-14 21:07:55 -0400

    delete when 0

--- pages/findgame/client/findgame.js
+++ pages/findgame/client/findgame.js
@@ -9,8 +9,16 @@ Template.showgame.events({
 		event.preventDefault();
 		console.log("clicked it");
 		console.dir(this);
+		if(this.g.need <= 1)
+		{
+			GameList.remove(this.g._id);
+
+		}
+		else
+		{
+
 		GameList.update(this.g._id,{$inc:{currentplays:1}})
 		GameList.update(this.g._id,{$inc:{need:-1}})
-
+		}
 	}
 })
