37a05765ea5f647ed97d2af2c1d15fb908a8c685
Veronica Gusev <cpgusev@gmail.com>
2016-07-15 09:50:33 -0400

    profile
    profile

---
+++ pages/myprofile/client/myprofile.js
@@ -0,0 +1,23 @@
+ const gender = $(".js-gender").val();
+ const birth = $(".js-birth").val();
+ const lifestyle = $(".js-lifestyle").val();
+ const ocd = $(".js-ocd").val();
+ const music =$(".js-music").val();
+ const subFree =$(".js-subFree").val();
+ const sleepHab =$(".js-sleepHab").val();
+ const sleepTime =$(".js-sleepTime").val();
+ const personality =$(".js-personality").val();
+ const pet =$(".js-pet").val();
+ const petYes =$(".js-petYes").val();
+ const animal =$(".js-animal").val();
+ const color =$(".js-color").val();
+ const hobby =$(".js-hobby").val();
+ const movie =$(".js-movie	").val();
+ const cook =$(".js-cook").val();
+ const sex =$(".js-sex").val();
+			
+   
+   
+ const  = {	};
+ console.dir(item);
+ UserInfo.insert(item);
