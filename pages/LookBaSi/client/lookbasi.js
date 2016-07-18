cfccec80c7bd4b313d140d00c1fd75242451a43f
High School Guest User <highschoolguest@turing.cs-i.brandeis.edu>
2016-07-18 15:16:17 -0400

    added a search facility

--- pages/LookBaSi/client/lookbasi.js
+++ pages/LookBaSi/client/lookbasi.js
@@ -1,11 +1,28 @@
-Template.lookbasi1.helpers({
-	BaSis: function() {return BaSis.find();},
+Session.set("zipcode",null);
 
+
+Template.lookbasi1.helpers({
+
+	BaSis: function() {
+	    const zipcode = Session.get("zipcode");
+	    if (zipcode==null || zipcode=="") 
+		return BaSis.find();
+	    if (zipcode!=null && zipcode != "")
+		return BaSis.find({zip:zipcode});
+       },
 
 })
 
+
 Template.lookbasi1.events({
+        "click .js-submit": function(event) {
+	    console.log("clicked it");
+	    Session.set("zipcode",$(".js-zipcode").val());
+	    console.log(Session.get("zipcode"));
+	},
+
 	"click .js-delete": function(event){
 		BaSis.remove(this.basi._id);
-	}
+	},
+
 })
